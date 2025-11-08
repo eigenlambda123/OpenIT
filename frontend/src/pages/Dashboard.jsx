d6fc778caad465f799ef45382d88ec8f4c42c2b4
neophiles <mnv.tagle@gmail.com>
2025-11-08 18:21:13 +0800

    feat: add dashboard components

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import {
+  Box, Flex,
+} from "@chakra-ui/react";
+import OngoingQuake from "../components/OngoingQuake/OngoingQuake";
+import RecentQuakes from "../components/RecentQuakes/RecentQuakes";
+import QuakeGuides from "../components/QuakeGuides/QuakeGuides";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <Box px={["20px", "30px"]} py={["10px", "20px"]} display="flex" flexDirection="column">    
+      <Flex direction="column" gap="20px">
+        <OngoingQuake />
+        <RecentQuakes />
+        <QuakeGuides />
+      </Flex>
+    </Box>
   );
 }
 
-export default Dashboard
+export default Dashboard;
