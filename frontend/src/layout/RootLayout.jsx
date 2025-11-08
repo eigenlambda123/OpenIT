523e26ccc8d095b1989f4220b45a698cc28888c5
neophiles <mnv.tagle@gmail.com>
2025-11-08 18:21:55 +0800

    add Flex styles

--- frontend/src/layout/RootLayout.jsx
+++ frontend/src/layout/RootLayout.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Flex } from "@chakra-ui/react";
 
 function RootLayout() {
 	return (
-		<>
+		<Flex
+      direction="column"
+      minH="100vh"
+    >
 			<Header />
 			<Outlet />
 			<Footer />
-		</>
+		</Flex>
 	);
 }
 
