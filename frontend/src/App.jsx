aea077fbc35759c94a7c6afb74a562de0af408bb
neophiles <mnv.tagle@gmail.com>
2025-11-09 01:34:50 +0800

    feat: add Intro useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -5,18 +6,39 @@ import {
   Navigate
 } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
+import Intro from './pages/Intro';
 import Dashboard from './pages/Dashboard';
 import Map from './pages/Map';
+import Settings from './pages/Settings'
 
 function AppRoutes() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={<RootLayout />} >
-        <Route index element={<Dashboard />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="map" element={<Map />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      {isLoading ? (
+        <>
+          <Route path="/start" element={<Intro />} />
+          <Route path="*" element={<Navigate to="/start" replace />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<RootLayout />} >
+            <Route index element={<Dashboard />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="map" element={<Map />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="map" element={<Map />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </>
+      )}
     </Routes>
   );
 }
