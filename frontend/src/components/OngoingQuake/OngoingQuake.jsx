27e1ee4bc464cfbc2c7b5ac1f87cf3f921991b50
neophiles <mnv.tagle@gmail.com>
2025-11-08 18:19:13 +0800

    feat: add initial OngoingQuake

---
+++ frontend/src/components/OngoingQuake/OngoingQuake.jsx
@@ -0,0 +1,60 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+  Box, Flex, Grid,
+  Heading, Text,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
+
+function OngoingQuake() {
+  return (
+    <Flex direction="column" gap="10px">
+      <Heading size={["sm", "md"]}>Happening Right Now</Heading>
+      <Flex
+        outline="black"
+        direction="column"
+        borderRadius="10px"
+        overflow="hidden"
+        bg="white"
+        sx={{filter: "drop-shadow(0px 0px 2px rgba(0, 0, 2, 0.3))"}}
+      >
+        <Box h="100px" w="100%" bg="gray.200">
+
+        </Box>
+        <Flex direction="column" gap="10px" p="10px">
+          <Heading size="sm">
+            Lucena City
+          </Heading>
+          <Grid
+            templateColumns="repeat(2, 1fr)"
+            gap="5px"
+            w="100%"
+          >
+            <Text fontSize="15px">
+              November 9, 2025
+            </Text>
+            <Text fontSize="sm">
+              7:00 PM
+            </Text>
+            <Text fontSize="sm">
+              5.5 Magnitude
+            </Text>
+            <Text fontSize="sm">
+              Lumikas!
+            </Text>
+          </Grid>
+        </Flex>
+        <HStack w="100%" p="10px">
+          <Button size={["sm", "md"]} w="50%">
+            View in Map
+          </Button>
+          <Button size={["sm", "md"]} w="50%">
+            Evacuate
+          </Button>
+        </HStack>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default OngoingQuake;
