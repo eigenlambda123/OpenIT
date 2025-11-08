262cede53358a94e615e879e9aa6df72e5b07f77
neophiles <mnv.tagle@gmail.com>
2025-11-08 21:58:28 +0800

    feat: add quakeData fetch useEffect

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import {
   Box, Flex,
 } from "@chakra-ui/react";
 import OngoingQuake from "../components/OngoingQuake/OngoingQuake";
 import RecentQuakes from "../components/RecentQuakes/RecentQuakes";
 import QuakeGuides from "../components/QuakeGuides/QuakeGuides";
+import { getEarthquakes } from "../api/quakes";
 
 function Dashboard() {
+  const [quakeData, setQuakeData] = useState([]);
+
+  useEffect(() => {
+    async function fetchEarthquakes() {
+      try {
+        const res = await getEarthquakes();
+        setQuakeData(res);
+      } catch (err) {
+        console.error("Failed to fetch earthquakes:", err);
+      }
+    }
+    fetchEarthquakes();
+  }, []);
+
   return (
     <Box px={["20px", "30px"]} py={["10px", "20px"]} display="flex" flexDirection="column">    
       <Flex direction="column" gap="20px">
         <OngoingQuake />
-        <RecentQuakes />
+        <RecentQuakes quakeData={quakeData} />
         <QuakeGuides />
       </Flex>
     </Box>
